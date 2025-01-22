14f0a59b43562efc0e24a6ca6b2c13b39652ff7c
Matthias Bürgler <buergler@gmx.ch>
2025-01-22 09:11:59 +0100

    execute code during automated doc building

--- docs/literate-script.jl
+++ docs/literate-script.jl
@@ -1,4 +1,4 @@
 using Literate
 
 directory_of_this_file = @__DIR__
-Literate.markdown("../scripts/NavierStokes3D.jl", directory_of_this_file, execute=false, documenter=true, credit=false)
+Literate.markdown("../scripts/NavierStokes3D.jl", directory_of_this_file, execute=true, documenter=true, credit=false)
