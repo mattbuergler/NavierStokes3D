c74a0f6e269d5bc239fc3562731848f124179c7e
Matthias Buergler <buergler@vaw.baug.ethz.ch>
2025-01-22 15:58:34 +0100

    attempt to fix tests

--- docs/literate-script.jl
+++ docs/literate-script.jl
@@ -1,4 +1,4 @@
 using Literate
 
 directory_of_this_file = @__DIR__
-Literate.markdown("../scripts/NavierStokes3D.jl", directory_of_this_file, execute=false, documenter=false, credit=false)
+Literate.markdown("../scripts/NavierStokes3D_multi_gpu.jl", directory_of_this_file, execute=false, documenter=false, credit=false)
--- test/runtests.jl
+++ test/runtests.jl
@@ -6,7 +6,7 @@ function runtests()
     exename = joinpath(Sys.BINDIR, Base.julia_exename())
     testdir = pwd()
 
-    printstyled("Testing NavierStokes3D.jl\n"; bold=true, color=:white)
+    printstyled("Testing NavierStokes3D_multi_gpu.jl\n"; bold=true, color=:white)
 
     run(`$exename -O3 --startup-file=no --check-bounds=no $(joinpath(testdir, "test3D.jl"))`)
     return
